f0670a175592f2d37cffdd40b1411551ef71183b
rahmams <rahma.maulida68@gmail.com>
2022-12-20 12:10:24 +0700

    Delete Apis File

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const fileUpload = require('express-fileupload');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var apisRouter = require('./routes/apis')
 
 var app = express();
 
